7090222eaf213a3ab97dfdae25f13a147a85d3f9
jj1guj <jj1guj@jj1guj.net>
2024-09-14 16:06:05 +0900

    fixed invalid path

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use cmake;
 
 fn main() {
-    let dst = cmake::build("../libdekunobou");
+    let dst = cmake::build("libdekunobou");
     println!("cargo:rustc-link-search=native={}", dst.display());
 
     println!("cargo:rustc-link-lib=static=dekunobou");
